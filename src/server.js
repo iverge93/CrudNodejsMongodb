8b7d5ebd63d5f194d9581b90200f43920150c19b
isaias vergelin <isaias.vergelin@ayi-asociados.com>
2021-04-24 16:35:10 -0300

    Adding layouts and partials. Also Boostrap css

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:  'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -14,7 +22,9 @@ app.use(express.urlencoded({extended: false}));
 // Global Variables
 
 // Routes
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
