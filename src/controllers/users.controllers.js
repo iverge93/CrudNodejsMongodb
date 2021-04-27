24e6b7cab8702d93d5c6d1c8fd6325ad19a67e29
isaias vergelin <isaias.vergelin@ayi-asociados.com>
2021-04-27 12:59:31 -0300

    some functionalities have been added on signup

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,12 +1,44 @@
 const userCtrl = {}
+const User = require('../models/User');
 
 userCtrl.renderSignUpForm = (req, res) => {
     res.render('users/signup');
 
 };
 
-userCtrl.signUp = (req, res) => {
-    res.send('signup');
+userCtrl.signUp = async (req, res) => {
+    const errors = [];
+    const {name, email, password, confirm_password} = req.body;
+
+    if(password != confirm_password){
+        errors.push({text: 'Passwords do not match'});
+    }
+    if(password.length < 8){
+        errors.push({text: 'Passwords must be at least 8 characters.'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup',  {
+            errors,
+            name,
+            email
+        });
+    }
+    else{
+       const  emailUser = await User.findOne({email: email});
+
+        if(emailUser){
+            req.flash('error_msg', 'The email is already in use');
+            res.redirect('/users/signup');
+        }
+        else{
+            const newUser = new User({name, email, password});
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'You are registered');
+            res.redirect('/users/signin');
+        }
+
+    }
 };
 
 userCtrl.renderSignInForm = (req, res) => {
--- src/database.js
+++ src/database.js
@@ -5,7 +5,8 @@ const MONGODB_URI = `mongodb://${HOST}/${DATABASE}`;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
     .then(db => console.log('Database is  connected'))
     .catch(err => console.log(err));
