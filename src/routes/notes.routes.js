c411890ff7db42860c95bcf2d9ea78a0146800a6
isaias vergelin <isaias.vergelin@ayi-asociados.com>
2021-04-28 23:48:27 -0300

    Routes have been protected

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,23 +1,29 @@
 const { Router } = require('express');
 const router = Router();
 
-const { renderNoteForm, createNewNote, renderNotes, 
-    renderEditForm, updateNote, deleteNote } = require('../controllers/notes.controller');
+const { renderNoteForm,
+        createNewNote,
+        renderNotes, 
+        renderEditForm,
+        updateNote,
+        deleteNote } = require('../controllers/notes.controller');
+
+const { isAuthenticated } = require('../helpers/auth');
 
 // new note
-router.get('/notes/add', renderNoteForm);
+router.get('/notes/add', isAuthenticated, renderNoteForm);
 
-router.post('/notes/new',createNewNote);
+router.post('/notes/new', isAuthenticated, createNewNote);
 
 // Get all notes
-router.get('/notes', renderNotes);
+router.get('/notes', isAuthenticated, renderNotes);
 
 // Edit notes
-router.get('/notes/edit/:id', renderEditForm);
+router.get('/notes/edit/:id', isAuthenticated, renderEditForm);
 
-router.put('/notes/edit/:id', updateNote);
+router.put('/notes/edit/:id', isAuthenticated, updateNote);
 
 // Delete notes
-router.delete('/notes/delete/:id', deleteNote);
+router.delete('/notes/delete/:id', isAuthenticated, deleteNote);
 
 module.exports = router;
